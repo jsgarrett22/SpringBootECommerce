4ad2ccbe5203c42164af8e172e409c73f607200e
Joshua Garrett <garrej01@email.franklin.edu>
2026-01-25 14:20:59 -0500

    Refactored for RestController. Implemented Login page controller scaffold.

--- src/main/java/com/garrett/SpringBootECommerce/controller/LoginController.java
+++ src/main/java/com/garrett/SpringBootECommerce/controller/LoginController.java
@@ -1,14 +1,13 @@
 package com.garrett.SpringBootECommerce.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HomeController {
-	
-	@GetMapping("/")
-	public String getHome() {
-		return "Home Page Test";
+public class LoginController {
+
+	@GetMapping("/login")
+	public String getLogin() {
+		return "Login Page";
 	}
 }
