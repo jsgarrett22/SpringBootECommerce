6c3d1e20cfe91e8ccd36ea5f508481c97f159500
Joshua Garrett <garrej01@email.franklin.edu>
2026-01-25 14:01:04 -0500

    initial commit

---
+++ src/test/java/com/garrett/SpringBootECommerce/SpringBootECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.garrett.SpringBootECommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
