2177c98eec30944b90ab50b2ee5a0f99cc56f331
Joshua Garrett <garrej01@email.franklin.edu>
2026-01-25 17:30:28 -0500

    Implemented product model

---
+++ src/main/java/com/garrett/SpringBootECommerce/model/Product.java
@@ -0,0 +1,40 @@
+package com.garrett.SpringBootECommerce.model;
+
+public class Product {
+	
+	private int productID;
+	private String productName;
+	private int price;
+	
+	public Product(int productID, String productName, int price) {
+		super();
+		this.productID = productID;
+		this.productName = productName;
+		this.price = price;
+	}
+
+	public int getProductID() {
+		return productID;
+	}
+
+	public void setProductID(int productID) {
+		this.productID = productID;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+}
